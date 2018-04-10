281517dfc3c5b3420bd3493ee58729cf50a00774
Adam Groszer <agroszer@gmail.com>
2018-04-10 15:13:03 +0200

    updates

--- setup.py
+++ setup.py
@@ -63,6 +63,7 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Framework :: ZODB',
         'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
